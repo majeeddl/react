837f424b93b73329e194a75cebc43e01d0cf4f5f
majeed <majeed.dl@gmail.com>
2022-01-23 23:35:47 +0300

    immutability libraries

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,31 +1,33 @@
-import { compose, pipe } from "lodash/fp";
+// import { compose, pipe } from "lodash/fp";
+import { Map } from "immutable";
+import { produce } from "immer";
 
-//trim
+// //trim
 
-let input = "    Javascript";
+// let input = "    Javascript";
 
-const trim = (str) => str.trim();
-const wrapInDiv = (str) => `<div>${str}</div>`;
-// const wrapInSpan = (str) => `<span>${str}</span>`;
+// const trim = (str) => str.trim();
+// const wrapInDiv = (str) => `<div>${str}</div>`;
+// // const wrapInSpan = (str) => `<span>${str}</span>`;
 
-// const wrap = (type, str) => `<${type}>${str}</${type}>`;
-//const transform = pipe(trim, toLowerCase, wrapInDiv("div"));
+// // const wrap = (type, str) => `<${type}>${str}</${type}>`;
+// //const transform = pipe(trim, toLowerCase, wrapInDiv("div"));
 
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
+// const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
 
-const toLowerCase = (str) => str.toLowerCase();
+// const toLowerCase = (str) => str.toLowerCase();
 
-let result = wrapInDiv(toLowerCase(trim(input)));
+// let result = wrapInDiv(toLowerCase(trim(input)));
 
-// const transform = compose(wrapInDiv,toLowerCase,trim)
-// const transform = pipe(trim, toLowerCase, wrapInDiv);
+// // const transform = compose(wrapInDiv,toLowerCase,trim)
+// // const transform = pipe(trim, toLowerCase, wrapInDiv);
 
-const transform = pipe(trim, toLowerCase, wrap("div"));
+// const transform = pipe(trim, toLowerCase, wrap("div"));
 
-result = transform(input);
+// result = transform(input);
 
-console.log(result);
-alert("hi");
+// console.log(result);
+// alert("hi");
 
 // const sayHello = ()=>{
 //     return ()=>{
@@ -40,3 +42,52 @@ alert("hi");
 // let numbers = [1,2,3]
 
 // numbers.map(number => number * 2 )
+
+const person = {
+  name: "John",
+  address: {
+    country: "USA",
+    city: "San Francisco",
+  },
+};
+//copy object
+const updated = Object.assign({}, person, { name: "Bob", age: 30 });
+console.log(updated);
+
+//This is a shallow copy
+const updatedTwo = { ...person, name: "Bob" };
+console.log(updatedTwo);
+
+const copyThree = {
+  ...person,
+  address: {
+    ...person.address,
+    city: "Washington",
+  },
+  name: "Bob",
+};
+console.log(copyThree);
+
+let book = Map({ title: "Hello" });
+
+console.log(book);
+console.log(book.get("title"));
+console.log(book.toJS());
+
+const publish = (book) => {
+  return book.set("isPublished", true);
+};
+
+book = publish(book);
+
+console.log(book.toJS());
+
+let book2 = { title: "Harry Potter" };
+
+const publishWithImmer = (book) => {
+  return produce(book, (draftBook) => {
+    draftBook.isPublished = true;
+  });
+};
+
+console.log(publishWithImmer(book2));
