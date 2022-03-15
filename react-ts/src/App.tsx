1418535d392a8970a83efdaa387aa99f2d82ed06
Majeed <majeed.dl@gmail.com>
2022-03-15 18:29:50 +0300

    working on redux

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Provider } from "react-redux";
 import {
   Route,
   Routes,
@@ -13,11 +13,13 @@ const Home = React.lazy(() => import("./Views/Home"));
 function App() {
   return (
     <div className="App">
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="*" element={<Home></Home>} />
-        </Routes>
-      </React.Suspense>
+      {/* <Provider store={store}> */}
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="*" element={<Home></Home>} />
+          </Routes>
+        </React.Suspense>
+      {/* </Provider> */}
     </div>
   );
 }
