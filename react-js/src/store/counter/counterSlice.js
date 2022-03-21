92caed96234c52a61ae6947f269efcfa5ea0ca5d
Majeed <majeed.dl@gmail.com>
2022-03-21 16:16:54 +0300

    working on redux toolkit

---
+++ react-js/src/store/counter/counterSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+    name : 'counter',
+    initialState,
+    reducers : {
+        // Redux toolkit use Immer,so we can change state without any concern
+        increment :(state)=>{
+            state.value+= 1;
+        },
+        decrement : (state)=>{
+            state.value-=1;
+        },
+        incrementByAmount : (state,action)=>{
+            state.value += action.payload;
+        }
+    }
+});
+
+export const { increment , decrement, incrementByAmount} = counterSlice;
+
+export default counterSlice.reducer;
