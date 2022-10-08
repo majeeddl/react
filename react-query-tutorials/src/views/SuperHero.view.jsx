8b044a83afdaecf8b417e36dae09f95c8c9794ff
majeed <majeed.dl@gmail.com>
2022-10-08 23:18:40 +0300

    read data with axios for super hero page

--- react-query-tutorials/src/views/SuperHero.view.jsx
+++ react-query-tutorials/src/views/SuperHero.view.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import axios from "axios"
 
 const SuperHeroview = () => {
+
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheroes").then((res) => {
+      setData(res.data)
+      setLoading(false)
+    })
+  }, [])
+
+  if (loading) {
+    return <h2>Loading ...</h2>
+  }
   return (
-    <div>superHeroview</div>
+    <>
+      <h2>
+        superHeroview
+      </h2>
+      <div>
+        {data.map((hero) => {
+          return <div key={hero.name}>{hero.name}</div>
+        })}
+      </div>
+    </>
   )
 }
 
