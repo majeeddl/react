6a9f3efac5e46e75ab1fc9937ab8c5b312c6ed33
Majeed Dourandeesh <majeed.dl@gmail.com>
2022-10-11 14:34:56 +0300

    add features on home page

--- react-table-examples/src/views/ColumnResizing.jsx
+++ react-table-examples/src/views/ColumnResizing.jsx
@@ -7,86 +7,11 @@ import {
   flexRender,
 } from '@tanstack/react-table'
 
+import { defaultColumns, defaultData } from '../utils/data'
+
 import './ColumnResizing.scss'
 
-const defaultData = [
-  {
-    firstName: 'tanner',
-    lastName: 'linsley',
-    age: 24,
-    visits: 100,
-    status: 'In Relationship',
-    progress: 50,
-  },
-  {
-    firstName: 'tandy',
-    lastName: 'miller',
-    age: 40,
-    visits: 40,
-    status: 'Single',
-    progress: 80,
-  },
-  {
-    firstName: 'joe',
-    lastName: 'dirte',
-    age: 45,
-    visits: 20,
-    status: 'Complicated',
-    progress: 10,
-  },
-]
 
-const defaultColumns = [
-  {
-    header: 'Name',
-    footer: props => props.column.id,
-    columns: [
-      {
-        accessorKey: 'firstName',
-        cell: info => info.getValue(),
-        footer: props => props.column.id,
-      },
-      {
-        accessorFn: row => row.lastName,
-        id: 'lastName',
-        cell: info => info.getValue(),
-        header: () => <span>Last Name</span>,
-        footer: props => props.column.id,
-      },
-    ],
-  },
-  {
-    header: 'Info',
-    footer: props => props.column.id,
-    columns: [
-      {
-        accessorKey: 'age',
-        header: () => 'Age',
-        footer: props => props.column.id,
-      },
-      {
-        header: 'More Info',
-        columns: [
-          {
-            accessorKey: 'visits',
-            header: () => <span>Visits</span>,
-            footer: props => props.column.id,
-          },
-          {
-            accessorKey: 'status',
-            header: 'Status',
-            footer: props => props.column.id,
-          },
-          {
-            accessorKey: 'progress',
-            header: 'Profile Progress',
-            footer: props => props.column.id,
-          },
-        ],
-      },
-    ],
-  },
-]
 
 const ColumnResizing = () => {
 
--- react-table-examples/src/views/Home.jsx
+++ react-table-examples/src/views/Home.jsx
@@ -1,8 +1,151 @@
-import React from 'react'
+import React, { useState, useReducer } from 'react'
+
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from '@tanstack/react-table'
+import { defaultColumns, defaultData } from '../utils/data'
+
+
 
 const Home = () => {
+
+  const [data, setData] = useState(() => [...defaultData])
+  const [columns] = useState(() => [
+    ...defaultColumns,
+  ])
+
+  const [columnResizeMode, setColumnResizeMode] = useState('onChange')
+  const [columnVisibility, setColumnVisibility] = React.useState({})
+
+  const rerender = useReducer(() => ({}), {})[1]
+
+  const table = useReactTable({
+    data,
+    columns,
+    columnResizeMode,
+    state: {
+      columnVisibility,
+    },
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: true,
+  })
+
+
   return (
-    <div>Home</div>
+    <>
+
+      <div className='mt-5'>This is a project that I want to show different features of 'react table' </div>
+
+      <div className='mt-2'>
+        <div className="inline-block border border-black shadow rounded">
+          <div className="px-1 border-b border-black">
+            <label>
+              <input
+                {...{
+                  type: 'checkbox',
+                  checked: table.getIsAllColumnsVisible(),
+                  onChange: table.getToggleAllColumnsVisibilityHandler(),
+                }}
+              />{' '}
+              Toggle All
+            </label>
+          </div>
+          {table.getAllLeafColumns().map(column => {
+            return (
+              <div key={column.id} className="px-1">
+                <label>
+                  <input
+                    {...{
+                      type: 'checkbox',
+                      checked: column.getIsVisible(),
+                      onChange: column.getToggleVisibilityHandler(),
+                    }}
+                  />{' '}
+                  {column.id}
+                </label>
+              </div>
+            )
+          })}
+        </div></div>
+      <div className='mt-2'>
+
+        <div className="overflow-x-auto">
+
+          <table
+            className='table table-bordered'
+            {...{
+              style: {
+                width: table.getCenterTotalSize(),
+              },
+            }}
+          >
+            <thead>
+              {table.getHeaderGroups().map(headerGroup => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map(header => (
+                    <th
+                      {...{
+                        key: header.id,
+                        colSpan: header.colSpan,
+                        style: {
+                          width: header.getSize(),
+                        },
+                      }}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      <div
+                        {...{
+                          onMouseDown: header.getResizeHandler(),
+                          onTouchStart: header.getResizeHandler(),
+                          className: `resizer ${header.column.getIsResizing() ? 'isResizing' : ''
+                            }`,
+                          style: {
+                            transform:
+                              columnResizeMode === 'onEnd' &&
+                                header.column.getIsResizing()
+                                ? `translateX(${table.getState().columnSizingInfo.deltaOffset
+                                }px)`
+                                : '',
+                          },
+                        }}
+                      />
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map(row => (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map(cell => (
+                    <td
+                      {...{
+                        key: cell.id,
+                        style: {
+                          width: cell.column.getSize(),
+                        },
+                      }}
+                    >
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   )
 }
 
