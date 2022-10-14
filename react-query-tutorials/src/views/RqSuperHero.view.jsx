a76f0b8cf5a085a0a1563a23df7f7225caf72aad
majeed <majeed.dl@gmail.com>
2022-10-14 20:35:00 +0300

    fetching data with useQuery

--- react-query-tutorials/src/views/RqSuperHero.view.jsx
+++ react-query-tutorials/src/views/RqSuperHero.view.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useQuery } from 'react-query'
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
 
 const RqSuperHeroview = () => {
-  return <div>RqSuperHeroview</div>;
+
+  const { isLoading, data } = useQuery('super-heroes',fetchSuperHeroes)
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  return <>
+    <h2> RQ Super Heroes</h2>
+    {data?.data.map((hero) => {
+      return <div key={hero.name}> {hero.name}</div>
+    })}
+  </>;
 };
 
 export default RqSuperHeroview;
