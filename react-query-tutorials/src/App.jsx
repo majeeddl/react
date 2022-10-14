346d21eaf1a0859763f674cc4816b8d71ed3f980
majeed <majeed.dl@gmail.com>
2022-10-14 20:25:25 +0300

    add query client provider

--- react-query-tutorials/src/App.jsx
+++ react-query-tutorials/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Route, Link, Routes } from "react-router-dom"
-
+import { QueryClientProvider, QueryClient} from "react-query"
 import { Navbar, Nav, Container } from "react-bootstrap"
 
 import './App.css'
@@ -12,27 +12,31 @@ import RqSuperHeroview from './views/RqSuperHero.view'
 function App() {
   const [count, setCount] = useState(0)
 
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <Navbar bg="light" expand="lg">
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link to="/" as={Link}>Home</Nav.Link>
+                <Nav.Link to="/super-heroes" as={Link}>super-heroes</Nav.Link>
+                <Nav.Link to="/rq-super-heroes" as={Link}>rq-super-heroes</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
         <Container>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link to="/" as={Link}>Home</Nav.Link>
-              <Nav.Link to="/super-heroes" as={Link}>super-heroes</Nav.Link>
-              <Nav.Link to="/rq-super-heroes" as={Link}>rq-super-heroes</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <Routes>
+            <Route path="/" exac element={<HomeView></HomeView>} ></Route>
+            <Route path="/super-heroes" element={<SuperHeroview></SuperHeroview>} ></Route>
+            <Route path="/rq-super-heroes" element={<RqSuperHeroview></RqSuperHeroview>} ></Route>
+          </Routes>
         </Container>
-      </Navbar>
-      <Container>
-        <Routes>
-          <Route path="/" exac element={<HomeView></HomeView>} ></Route>
-          <Route path="/super-heroes" element={<SuperHeroview></SuperHeroview>} ></Route>
-          <Route path="/rq-super-heroes" element={<RqSuperHeroview></RqSuperHeroview>} ></Route>
-        </Routes>
-      </Container>
-    </div>
+      </div>
+    </QueryClientProvider>
   )
 }
 
