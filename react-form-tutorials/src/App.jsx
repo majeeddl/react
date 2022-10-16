e33446c2bbd73c2d534c375ef9fa9abd2f1e1130
majeed <majeed.dl@gmail.com>
2022-10-16 21:45:28 +0300

    #24 : basic example for react form

--- react-form-tutorials/src/App.jsx
+++ react-form-tutorials/src/App.jsx
@@ -1,13 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Link, Routes, Route } from "react-router-dom"
+import { Navbar, Nav, Container } from "react-bootstrap"
 import './App.css'
+import Home from './views/Home'
+import Validation from './views/Validation'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link to="/" as={Link}>Home</Nav.Link>
+                <Nav.Link to="/validation" as={Link}>validation</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Container className='p-10'>
+          <Routes>
+            <Route path="/" exac element={<Home></Home>} ></Route>
+            <Route path="/validation" element={<Validation></Validation>} ></Route>
+            {/* <Route path="/super-heroes" element={<SuperHeroview></SuperHeroview>} ></Route>
+            <Route path="/rq-super-heroes" element={<RqSuperHeroview></RqSuperHeroview>} ></Route> */}
+          </Routes>
+        </Container>
+      </div>
     </div>
   )
 }
