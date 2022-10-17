658521a52051720bd2e93de524dc3cbec44e157e
majeed <majeed.dl@gmail.com>
2022-10-17 21:51:52 +0300

    add validation in react hook form

--- react-form-tutorials/src/views/Validation.jsx
+++ react-form-tutorials/src/views/Validation.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const Validation = () => {
+
+  const { handleSubmit, register } = useForm()
+
+  const onSubmit = data => console.log(data)
+
   return (
-    <div>Validation</div>
+    <>
+      <div>Validation</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("firstName", { required: true, maxLength: 20 })} />
+        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+        <input type="number" {...register('age', { min: 18, max: 99 })} />
+        <input type="submit" />
+      </form>
+    </>
   )
 }
 
