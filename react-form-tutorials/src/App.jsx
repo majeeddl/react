0d1967ab2c290cb0c1fced620d2256462538e387
Majeed Dourandeesh <majeed.dl@gmail.com>
2022-10-18 17:23:53 +0300

    #30 : handling errors in react hook form

--- react-form-tutorials/src/App.jsx
+++ react-form-tutorials/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Home from './views/Home'
 import Validation from './views/Validation'
 import Integrating from './views/Integrating'
+import ErrorHandling from './views/ErrorHandling'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
                 <Nav.Link to="/" as={Link}>Home</Nav.Link>
                 <Nav.Link to="/validation" as={Link}>Validation</Nav.Link>
                 <Nav.Link to="/integrating" as={Link}>Integrating</Nav.Link>
+                <Nav.Link to="/handling-error" as={Link}>Handling Error</Nav.Link>
                 <Nav.Link to="/integration-antd" as={Link}>Integration Antd</Nav.Link>
               </Nav>
             </Navbar.Collapse>
@@ -29,6 +31,7 @@ function App() {
             <Route path="/" exac element={<Home></Home>} ></Route>
             <Route path="/validation" element={<Validation></Validation>} ></Route>
             <Route path="/integrating" element={<Integrating></Integrating>} ></Route>
+            <Route path="/handling-error" element={<ErrorHandling></ErrorHandling>} ></Route>
             {/* <Route path="/super-heroes" element={<SuperHeroview></SuperHeroview>} ></Route>
             <Route path="/rq-super-heroes" element={<RqSuperHeroview></RqSuperHeroview>} ></Route> */}
           </Routes>
---
+++ react-form-tutorials/src/views/ErrorHandling.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const ErrorHandling = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <>
+      <div>ErrorHandling</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("firstName", { required: true })}
+          aria-invalid={errors.firstName ? "true" : "false"}
+        />
+        {errors.firstName?.type === "required" && (
+          <p role="alert">First name is required</p>
+        )}
+
+        <input
+          {...register("mail", { required: "Email Address is required" })}
+          aria-invalid={errors.mail ? "true" : "false"}
+        />
+        {errors.mail && <p role="alert">{errors?.mail?.message}</p>}
+
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
+
+export default ErrorHandling;
