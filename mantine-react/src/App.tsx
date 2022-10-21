79ef33bd223b7b1fddfb05e11d4df068336ea41a
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-10-21 09:39:50 +0300

    #35 : basic of mantine

--- mantine-react/src/App.tsx
+++ mantine-react/src/App.tsx
@@ -1,14 +1,79 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import {Route,Routes} from "react-router-dom"
+import { Container } from "@mantine/core"
+import "./App.css";
+import HeaderAction, { HeaderActionProps } from "./components/HeaderAction";
+import Home from "./views/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const _links: any = {
+    links: [
+      {
+        link: "/",
+        label: "Basic",
+      },
+      {
+        link: "#1",
+        label: "Learn",
+        links: [
+          {
+            link: "/docs",
+            label: "Documentation",
+          },
+          {
+            link: "/resources",
+            label: "Resources",
+          },
+          {
+            link: "/community",
+            label: "Community",
+          },
+          {
+            link: "/blog",
+            label: "Blog",
+          },
+        ],
+      },
+      {
+        link: "/about",
+        label: "About",
+      },
+      {
+        link: "/pricing",
+        label: "Pricing",
+      },
+      {
+        link: "#2",
+        label: "Support",
+        links: [
+          {
+            link: "/faq",
+            label: "FAQ",
+          },
+          {
+            link: "/demo",
+            label: "Book a demo",
+          },
+          {
+            link: "/forums",
+            label: "Forums",
+          },
+        ],
+      },
+    ],
+  };
 
   return (
     <div className="App">
-      
+      <HeaderAction {..._links} />
+      <Container className="p-10">
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+        </Routes>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- mantine-react/src/main.tsx
+++ mantine-react/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { MantineProvider } from "@mantine/core";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <MantineProvider
+    theme={{
+      fontFamily: "Open Sans",
+      spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+    }}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MantineProvider>
+);
