86d08a40992fd70464ab1264dd36788665ae0ff2
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-10-24 12:54:12 +0300

    change send paramter to function

--- react-query-tutorials/src/hooks/useSuperHeroData.jsx
+++ react-query-tutorials/src/hooks/useSuperHeroData.jsx
@@ -1,10 +1,11 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
-const fetchSuperHeroById = (heroId) => {
+const fetchSuperHeroById = ({ queryKey }) => {
+  const heroId = queryKey[1];
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
 
 export const useSuperHeroData = (heroId) => {
-  return useQuery(["super-hero", heroId], () => fetchSuperHeroById(heroId));
+  return useQuery(["super-hero", heroId], fetchSuperHeroById);
 };
