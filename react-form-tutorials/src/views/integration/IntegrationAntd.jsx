a3f315247ad5038da66e4d9c096281fa792451fe
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-10-26 11:23:32 +0300

    #25 :  integrate with antd

--- react-form-tutorials/src/views/integration/IntegrationAntd.jsx
+++ react-form-tutorials/src/views/integration/IntegrationAntd.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { Alert, Stack } from "react-bootstrap";
+import { Input, Select } from "antd";
+import { Controller, useForm } from "react-hook-form";
+const { Option } = Select;
 
 const IntegrationAntd = () => {
-  return <div>IntegrationAntd</div>;
+  const {
+    control,
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      age: 36,
+      select: {},
+    },
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <>
+      <div className="mt-5">IntegrationAntd</div>
+      <div className="mt-2">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack gap={2}>
+            <Controller
+              name="firstName"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <>
+                  {console.log(errors)}
+                  <Input
+                    status={errors.firstName ? "error" : ""}
+                    {...field}
+                  ></Input>
+                </>
+              )}
+            ></Controller>
+            {errors.firstName?.type === "required" && (
+              <Alert variant="danger" size="sm" className="mt-2">
+                First Name is required
+              </Alert>
+            )}
+
+            <Controller
+              name="select"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <>
+                  <Select defaultValue="lucy" style={{ width: 120 }} {...field}>
+                    <Option value="jack">Jack</Option>
+                    <Option value="lucy">Lucy</Option>
+                    <Option value="disabled" disabled>
+                      Disabled
+                    </Option>
+                    <Option value="Yiminghe">yiminghe</Option>
+                  </Select>
+                </>
+              )}
+            ></Controller>
+
+            <input className="mt-2" type="submit"></input>
+          </Stack>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default IntegrationAntd;
