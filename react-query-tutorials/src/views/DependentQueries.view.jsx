173146ce93c413478750afa5e7302fd10531104e
majeed <majeed.dl@gmail.com>
2022-10-28 19:52:08 +0300

    #49 : dependent queies in react query

--- react-query-tutorials/src/views/DependentQueries.view.jsx
+++ react-query-tutorials/src/views/DependentQueries.view.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import axios from 'axios'
 
-const DependentQueriesView = () => {
-  return (
-    <div>DependentQueriesView</div>
-  )
+import { useQuery } from 'react-query'
+
+const fetchUserByEmail = (email) => {
+    return axios.get(`http://localhost:4000/users/${email}`)
+}
+
+const fetchCoursesByChannelId = channelId => {
+    return axios.get(`http://localhost:4000/channels/${channelId}`)
+}
+
+const DependentQueriesView = ({ email }) => {
+
+    const { data: user } = useQuery(['user', email], () => fetchUserByEmail(email))
+
+    const channelId = user?.data?.channelId
+
+    useQuery(['courses', channelId], () => fetchCoursesByChannelId(channelId), {
+        enabled: !!channelId
+    })
+
+    return (
+        <div>DependentQueriesView</div>
+    )
 }
 
 export default DependentQueriesView
