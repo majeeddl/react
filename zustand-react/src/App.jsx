afca1fd4b7e517374398bd22c1f58ae1788b29e3
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-23 14:20:10 +0300

    #60 : use arrays in zustand

--- zustand-react/src/App.jsx
+++ zustand-react/src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "./App.css";
 import { setState, useStore } from "./store/store";
 
@@ -5,6 +6,14 @@ function App() {
   const getVotes = useStore((state) => state.votes);
   const fetch = useStore((state) => state.fetch);
   const addVotes = useStore((state) => state.addVotes);
+
+  const fruits = useStore((state) => state.fruits);
+  const addFruits = useStore((state) => state.addFruits);
+  const inputRef = useRef();
+  const addFruit = () => {
+    addFruits(inputRef.current.value);
+    inputRef.current.value = "";
+  };
 
   return (
     <div className="App">
@@ -20,6 +29,12 @@ function App() {
       >
         Fetch votes
       </button>
+
+      <input ref={inputRef} />
+      <button onClick={addFruit}>Add a fruit</button>
+      {fruits.map((fruit) => (
+        <p key={fruit}>{fruit}</p>
+      ))}
     </div>
   );
 }
