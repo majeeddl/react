9cf709aaef74b8ae2c0bc97def54e8656b7ff39c
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-23 14:11:35 +0300

    #59 : working with fetch data zustand

--- zustand-react/src/App.jsx
+++ zustand-react/src/App.jsx
@@ -3,7 +3,7 @@ import { setState, useStore } from "./store/store";
 
 function App() {
   const getVotes = useStore((state) => state.votes);
-
+  const fetch = useStore((state) => state.fetch);
   const addVotes = useStore((state) => state.addVotes);
 
   return (
@@ -11,6 +11,15 @@ function App() {
       <div> votes is : {getVotes}</div>
 
       <button onClick={addVotes}> increase vote </button>
+
+      <div> votes is : {getVotes.length}</div>
+      <button
+        onClick={() => {
+          fetch();
+        }}
+      >
+        Fetch votes
+      </button>
     </div>
   );
 }
