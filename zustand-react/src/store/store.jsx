b01c5154e5ffa23d8dac5ba956f06bc6204ec2cd
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-23 14:27:25 +0300

    #61 : use devtools for zustand

--- zustand-react/src/store/store.jsx
+++ zustand-react/src/store/store.jsx
@@ -1,27 +1,30 @@
 import create, { createStore } from "zustand";
+import { devtools } from "zustand/middleware";
 
 const voting = "https://api.github.com/search/users?q=john&per_page=5";
 
-export const useStore = create((set) => ({
-  votes: voting,
-  Votes: {},
-  addVotes: () => set((state) => ({ votes: state.votes + 1 })),
+export const useStore = create(
+  devtools((set) => ({
+    votes: voting,
+    Votes: {},
+    addVotes: () => set((state) => ({ votes: state.votes + 1 })),
 
-  fruits: ["apple", "banana", "orange"],
-  addFruits: (fruit) => {
-    set((state) => ({
-      fruits: [...state.fruits, fruit],
-    }));
-  },
+    fruits: ["apple", "banana", "orange"],
+    addFruits: (fruit) => {
+      set((state) => ({
+        fruits: [...state.fruits, fruit],
+      }));
+    },
 
-  fetch: async (voting) => {
-    const response = await fetch(voting);
-    const json = await response.json();
+    fetch: async (voting) => {
+      const response = await fetch(voting);
+      const json = await response.json();
 
-    set({
-      Votes: json.items,
-    });
-  },
-}));
+      set({
+        Votes: json.items,
+      });
+    },
+  }))
+);
 
 export const { getState, setState, subscribe } = useStore;
