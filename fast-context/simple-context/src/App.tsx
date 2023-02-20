73ee173c614ed72223902fc4798ee69a0191fda9
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-02-20 10:52:54 +0300

    init simple context project

---
+++ fast-context/simple-context/src/App.tsx
@@ -0,0 +1,61 @@
+
+import "./App.css";
+
+const TextInput = ({ value }: { value: "first" | "last" }) => {
+  return (
+    <div className="field">
+      {value}: <input />
+    </div>
+  );
+};
+
+const Display = ({ value }: { value: "first" | "last" }) => {
+  return (
+    <div className="value">
+      {value}: {""}
+    </div>
+  );
+};
+
+const FormContainer = () => {
+  return (
+    <div className="container">
+      <h5>FormContainer</h5>
+      <TextInput value="first" />
+      <TextInput value="last" />
+    </div>
+  );
+};
+
+const DisplayContainer = () => {
+  return (
+    <div className="container">
+      <h5>DisplayContainer</h5>
+      <Display value="first" />
+      <Display value="last" />
+    </div>
+  );
+};
+
+const ContentContainer = () => {
+  return (
+    <div className="container">
+      <h5>ContentContainer</h5>
+      <FormContainer />
+      <DisplayContainer />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h5>App</h5>
+        <ContentContainer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
