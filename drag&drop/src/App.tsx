db7b2f4173a1a940d1bf5d681ce9f117cd06792a
majeed <majeed.dl@gmail.com>
2023-03-10 11:12:26 +0300

    init basic draggable with dnd kit

--- drag&drop/src/App.tsx
+++ drag&drop/src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { DndContext } from "@dnd-kit/core";
+import Draggable from "./Draggable";
+import Droppable from "./Droppable";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-     
+      <DndContext>
+        <Draggable>
+          <div>draggable</div>
+        </Draggable>
+        <Droppable />
+      </DndContext>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
