27e09b68f726939b2699959fcb8ca5efa1136222
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-08-11 10:39:46 +0300

    add basic table

--- mantine-react-table/src/App.tsx
+++ mantine-react-table/src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo } from "react";
+import {
+  MantineReactTable,
+  useMantineReactTable,
+  type MRT_ColumnDef, //if using TypeScript (optional, but recommended)
+} from "mantine-react-table";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+//If using TypeScript, define the shape of your data (optional, but recommended)
+interface Person {
+  name: string;
+  age: number;
 }
 
-export default App
+//mock data - strongly typed if you are using TypeScript (optional, but recommended)
+const data: Person[] = [
+  {
+    name: "John",
+    age: 30,
+  },
+  {
+    name: "Sara",
+    age: 25,
+  },
+];
+
+export default function App() {
+  //column definitions - strongly typed if you are using TypeScript (optional, but recommended)
+  const columns = useMemo<MRT_ColumnDef<Person>[]>(
+    () => [
+      {
+        accessorKey: "name", //simple recommended way to define a column
+        header: "Name",
+        mantineTableHeadCellProps: { sx: { color: "green" } }, //custom props
+      },
+      {
+        accessorFn: (originalRow) => originalRow.age, //alternate way
+        id: "age", //id required if you use accessorFn instead of accessorKey
+        header: "Age",
+        Header: <i style={{ color: "red" }}>Age</i>, //optional custom markup
+      },
+    ],
+    []
+  );
+
+  //pass table options to useMantineReactTable
+  const table = useMantineReactTable({
+    columns,
+    data, //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+    enableRowSelection: true, //enable some features
+    enableColumnOrdering: true,
+    enableGlobalFilter: false, //turn off a feature
+  });
+
+  //note: you can also pass table options as props directly to <MantineReactTable /> instead of using useMantineReactTable
+  //but that is not recommended and will likely be deprecated in the future
+  return <MantineReactTable table={table} />;
+}
