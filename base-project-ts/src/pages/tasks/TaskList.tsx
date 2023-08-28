a5e9d75703cb7fa0dda07ef5550849de89ed4cb2
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-08-28 14:11:40 +0300

    change sth

--- base-project-ts/src/pages/tasks/TaskList.tsx
+++ base-project-ts/src/pages/tasks/TaskList.tsx
@@ -2,87 +2,87 @@ import { useState } from "react";
 import { useTasks, useTasksDispatch } from "./TasksContext";
 
 export default function TasksList() {
-    const tasks = useTasks();
+  const tasks = useTasks();
 
-    return (
-        <ul>
-            {tasks?.map((task: any) => (
-                <li key={task.id}>
-                    <Task task={task} />
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {tasks?.map((task: any) => (
+        <li key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 const Task = ({ task }: any) => {
-    const [isEditing, setEditing] = useState(false);
-    const dispatch = useTasksDispatch();
+  const [isEditing, setEditing] = useState(false);
+  const dispatch: any = useTasksDispatch();
 
-    let taskContent;
-    if (isEditing) {
-        taskContent = (
-            <>
-                <input
-                    type="text"
-                    value={task.text}
-                    onChange={(e) => {
-                        if (task) {
-                            dispatch({
-                                type: "changed",
-                                payload: {
-                                    id: task.id,
-                                    text: e.target.value,
-                                },
-                            });
-                        }
-                    }}
-                />
-                <button onClick={() => setEditing(false)}>Done</button>
-            </>
-        );
-    } else {
-        taskContent = (
-            <>
-                <span>{task.text}</span>
-                <button onClick={() => setEditing(true)}>Edit</button>
-            </>
-        );
-    }
-
-    return (
-        <label>
-            <input
-                type="checkbox"
-                checked={task.done}
-                onChange={(e) => {
-                    if (task) {
-                        dispatch({
-                            type: "changed",
-                            payload: {
-                                id: task.id,
-                                text: task.text,
-                                done: e.target.checked,
-                            },
-                        });
-                    }
-                }}
-            />
-            {taskContent}
-            <button
-                onClick={() => {
-                    if (task) {
-                        dispatch({
-                            type: "deleted",
-                            payload: {
-                                id: task.id,
-                            },
-                        });
-                    }
-                }}
-            >
-                Delete
-            </button>
-        </label>
+  let taskContent;
+  if (isEditing) {
+    taskContent = (
+      <>
+        <input
+          type="text"
+          value={task.text}
+          onChange={(e) => {
+            if (task) {
+              dispatch({
+                type: "changed",
+                payload: {
+                  id: task.id,
+                  text: e.target.value,
+                },
+              });
+            }
+          }}
+        />
+        <button onClick={() => setEditing(false)}>Done</button>
+      </>
     );
+  } else {
+    taskContent = (
+      <>
+        <span>{task.text}</span>
+        <button onClick={() => setEditing(true)}>Edit</button>
+      </>
+    );
+  }
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={task.done}
+        onChange={(e) => {
+          if (task) {
+            dispatch({
+              type: "changed",
+              payload: {
+                id: task.id,
+                text: task.text,
+                done: e.target.checked,
+              },
+            });
+          }
+        }}
+      />
+      {taskContent}
+      <button
+        onClick={() => {
+          if (task) {
+            dispatch({
+              type: "deleted",
+              payload: {
+                id: task.id,
+              },
+            });
+          }
+        }}
+      >
+        Delete
+      </button>
+    </label>
+  );
 };
